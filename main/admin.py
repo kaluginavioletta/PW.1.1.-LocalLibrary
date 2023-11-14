571ad5576461db0e828c0d7ab03c2de2d337a059
kaluginavioletta <kalugina7bk.ru@yandex.ru>
2023-11-14 09:18:08 +0700

    Displaying lists and detailed information

--- main/admin.py
+++ main/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from .models import Author, Genre, Book, BookInstance
+from .models import Author, Genre, Book, BookInstance, Language
 
 # admin.site.register(Book)
-admin.site.register(Author)
+# admin.site.register(Author)
 admin.site.register(Genre)
+admin.site.register(Language)
 # admin.site.register(BookInstance)
 
 
@@ -14,13 +15,19 @@ class AuthorAdmin(admin.ModelAdmin):
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
 # Register the admin class with the associated model
-# admin.site.register(Author, AuthorAdmin)
+admin.site.register(Author, AuthorAdmin)
+
+
 
 # Register the Admin classes for Book using the decorator
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
     def display_genre(self):
         """
         Creates a string for the Genre. This is required to display genre in Admin.
